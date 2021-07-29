58ef0edbad0728072847d076a3882907d2ebc7cd
WenRan6 <88079244+WenRan6@users.noreply.github.com>
2021-07-29 16:35:57 +0800

    Add files via upload
    第一次上传

---
+++ test.c
@@ -0,0 +1,283 @@
+//#include<stdio.h>
+//int main()
+//{
+	//char ch='A';
+	//printf("%c\n",ch);
+	//int age=20;
+	//printf("%d\n",age);
+		//printf("%d\n",sizeof(char));
+	
+	//return 0;
+//}
+//int main()
+//{
+//	printf("%d\n",sizeof(char));
+//return 0;
+//}
+//int main()
+//{
+//	printf("%d\n",sizeof(short));
+//return 0;
+//}
+//int main()
+//{
+	//printf("%d\n",sizeof(int));
+//return 0;
+//}
+//int main()
+//{
+	//printf("%d\n",sizeof(long));
+	//return 0;
+//}
+//int main()
+//{printf("%d\n",sizeof(long long));
+//return 0;}
+//int main()
+//{
+	//printf("%d\n",sizeof(float));
+	//return 0 ;
+//}
+//int main()
+//{
+//printf("%d\n",sizeof(double));
+//return 0;
+//}
+//int main()
+//{
+//	printf("%d\n",sizeof(char));//
+//	printf("%d\n",sizeof(short));//
+//	printf("%d\n",sizeof(int));//
+//	printf("%d\n", sizeof(long));//
+//	printf("%d\n",sizeof(long long));//
+//	printf("%d\n",sizeof(float));//
+//	printf("%d\n",sizeof(double));//
+//	return 0;
+//}
+//int main()
+//{
+//	int num1;
+//		int num2;
+//		int sum;
+//		scanf("%d%d",&num1,&num2);
+//		
+//		sum=num1+num2;
+//		printf("sum=%d\n",sum);
+//		return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	char ch='a';
+//	printf("%c\n",ch);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main
+//	()
+//{
+//int lll=170;
+//	
+//	printf("%d\n",lll);
+//return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	int age=20;
+//	printf("%d\n",age);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	long num=100;
+//	printf("%d\n",num);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	float weight =58.5f;
+//	printf("%f\n",weight);
+//	
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	double price=1.1;
+//	printf("%f\n",price);
+//	return 0;
+//}
+	//#include<stdio.h>
+	//int main()
+	//{
+	//printf("%d\n",sizeof(char));
+	//return 0;
+	//}
+//#include<stdio.h>
+//int main()
+//{
+//	printf("%d\n",sizeof(short));
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	printf("%d\n",sizeof(int));
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	printf("%d\n",sizeof(long));
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	printf("%d\n",sizeof(long long));
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	printf("%d\n",sizeof(float));
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	printf("%d\n",sizeof(double));
+//	return 0;
+//
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int num1;
+//	int num2;
+//	int sum;
+//	scanf("%d%d",&num1,&num2);
+//	sum=num1+num2;
+//	printf("sum=%d\n",sum);
+//	return 0;
+//}
+//	
+//#include<stdio.h>
+//int main()
+//{
+//	float weight=58.5f;
+//	return 0;
+//}*/
+//#include<stdio.h>
+//int num=18;
+//	int main()
+//{
+//	
+//	printf("%d\n",num);
+//	return 0;
+//}
+//
+//#include<stdio.h>
+//int main()
+//{	int num1;
+//int num2;
+//int sum;
+//scanf("%d%d",&num1,&num2);
+//sum=num1+num2;
+//printf("sum=%d\n",sum);
+//return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	//printf("Hello world!");
+//	return 0;
+////}
+////#include<stdio.h>
+////int main()
+////{
+////	printf("Hello world!");
+////	return 0;
+////}
+//#include<stdio.h>
+//int main()
+//{
+//	printf("Hello world!\n");
+//	return 0;
+//}
+//#include<stdio.h>
+//int b=10;
+//int main()
+//{
+//	int a=1;
+//	printf("%d\n",b);
+//	return 0;
+//}
+#include<stdio.h>
+//int main()
+//{
+//	int num1=3;
+//	int num2=5;
+//	int num3=6;
+//	int sum;
+//	scanf("%d%d%d",&num1,&num2,&num3);
+//		sum=num1+num2+num3;
+////		printf("sum=%d\n",sum);
+////		return 0;
+////}
+//#include<stdio.h>
+//int main()
+//{
+//	int a=9;
+//	{printf("%d\n",a);}
+//	return 0;
+//}
+//#include<stdio.h>
+// int global=2021;
+// void test()
+// {printf("test()--%d\n",global);
+// }
+//int main()
+//{
+//	test();
+//	printf("%d\n",global);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	extern int g_val;
+//	printf("g_val=%d\n",g_val);
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
